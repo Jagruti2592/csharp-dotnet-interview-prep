45f711f8094b02ad4925ed8bd2a5afc60363b4d6
Jagruti Chitte <81913740+Jagruti2592@users.noreply.github.com>
2026-04-14 21:12:57 -0700

    new MVC app.

---
+++ MyApp/MyApp/Controllers/ItemsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.Models;
+
+namespace MyApp.Controllers
+{
+    public class ItemsController : Controller
+    {
+        public IActionResult Overview()
+        {
+            var items = new Item()
+            {
+                //Id = 1,
+                Name = "Sample Item"
+                //Price = 9.99m
+            };
+            return View(items );
+        }
+    }
+}
