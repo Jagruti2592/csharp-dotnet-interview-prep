eabb4c4b28b6b554b89f0bf2a3896d29599240ba
Jagruti Chitte <81913740+Jagruti2592@users.noreply.github.com>
2026-04-17 16:33:06 -0700

    Add validation on client and server side for categories.

--- BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
@@ -18,9 +18,28 @@ namespace BulkyWeb.Controllers
             return View(objCategoryList);
         }
 
-        public IActionResult Create() {
+        public IActionResult Create()
+        {
 
             return View();
         }
+
+        [HttpPost]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The Display Order cannot exactly match the Name.");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index", "Category");
+
+            }
+            return View();
+        }
+
     }
 }
